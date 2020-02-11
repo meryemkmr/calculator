db64a0d1e000bf3e879f408b88ff795279699dd9
meryemkmr <meryemkmr36@gmail.com>
2020-02-11 16:55:46 -0600

    working on it

---
+++ js/script2.js
@@ -0,0 +1,8 @@
+let buttons = document.querySelector('.buttons');
+let result = document.getElementById('result');
+let input = document.getElementById('input');
+let numStorage = [];
+let stringStorage = [];
+
+let sum = [];
+
